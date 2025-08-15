8dde910d492ce63d18f6309fb5783540215646ff
Nuykevinazmi <161460159+Nuykevinazmi@users.noreply.github.com>
2025-08-16 02:15:15 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,4 +1,4 @@
-const ALLOWED_ORIGIN = "https://f7f62a8a.itachi-aev.pages.dev"; // Ganti dengan domain frontend kamu
+const ALLOWED_ORIGIN = "https://7a902e57.itachi-aev.pages.dev"; // Ganti dengan domain frontend kamu
 
 export default {
   async fetch(request, env) {
