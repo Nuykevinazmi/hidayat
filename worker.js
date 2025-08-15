9df01db342e6ea816f74e783551f9ed6d953c58f
Nuykevinazmi <161460159+Nuykevinazmi@users.noreply.github.com>
2025-08-15 16:26:03 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,4 +1,4 @@
-const ALLOWED_ORIGIN = "https://mlbbvalid.pages.dev"; // Ganti dengan domain frontend kamu
+const ALLOWED_ORIGIN = "https://arif2.pages.dev"; // Ganti dengan domain frontend kamu
 
 export default {
   async fetch(request, env) {
