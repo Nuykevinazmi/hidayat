a1e332f5ea633f66d5402a117e414856f30ada3d
Nuykevinazmi <161460159+Nuykevinazmi@users.noreply.github.com>
2025-08-15 18:37:09 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,4 +1,4 @@
-const ALLOWED_ORIGIN = "https://arif-81a.pages.dev"; // Ganti dengan domain frontend kamu
+const ALLOWED_ORIGIN = "https://242ba096.arif-81a.pages.dev"; // Ganti dengan domain frontend kamu
 
 export default {
   async fetch(request, env) {
